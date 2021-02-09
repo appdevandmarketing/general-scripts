15e09f150706fd18f517d10dacbfc1fd191dcd59
Dargonit <igrokyse@gmail.com>
2021-02-09 14:11:52 -0800

    changed function place

--- general-script.js
+++ general-script.js
@@ -112,10 +112,10 @@ function getParameterByName(name) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-if (typeof getParameterByName("") == 'function') {
+console.log("before gen cookie");
 //set cookie
-    if (getParameterByName('dsp') == "F70624" || getParameterByName('dsp') == "FACD02" || getParameterByName('dsp') == "010101") {
-        trackUrlParameters();
-        console.log(clickUrlParameterId);
-    }
+if (getParameterByName('dsp') == "F70624" || getParameterByName('dsp') == "FACD02" || getParameterByName('dsp') == "010101") {
+    trackUrlParameters();
+    console.log(clickUrlParameterId);
 }
+
