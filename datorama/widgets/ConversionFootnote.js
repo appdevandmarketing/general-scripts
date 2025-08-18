340ff98b7089be1b2a452cb0bb3b6e9a63078e5a
Sujan Maharjan <sujan.maharjan555@gmail.com>
2025-08-18 11:55:12 +0545

    Change note : july -> August

--- datorama/widgets/ConversionFootnote.js
+++ datorama/widgets/ConversionFootnote.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" style="fill: rgba(0, 170, 255, 1);transform: ;msFilter:;"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path><path d="M11 11h2v6h-2zm0-4h2v2h-2z"></path></svg>
         <div class="custom-popover">
         <b> Disclaimer: </b> <br/>
-The APS category was implemented in March/April 2025 and remained in beta until the end of July 2025. If a date range prior to implementation is selected, all conversions will appear as "uncategorized" and categories themselves may not be accurate or consistent during the beta period.</div>
+The APS category was implemented in March/April 2025 and remained in beta until the end of August 2025. If a date range prior to implementation is selected, all conversions will appear as "uncategorized" and categories themselves may not be accurate or consistent during the beta period.</div>
      
      </div>
       <u>Conversion Category Descriptions:</u>
